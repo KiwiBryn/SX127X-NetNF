e5279a2d2d8e30ab7e2c6218246f6ff5efc1d8f4
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2022-03-11 17:17:02 +1300

    Stubbed out device library and test client

---
+++ SX127XLoRaDevice/SX127XDevice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SX127XLoRaDevice
+{
+	public class SX127XLoRaDevice
+	{
+		// Browse our samples repository: https://github.com/nanoframework/samples
+		// Check our documentation online: https://docs.nanoframework.net/
+		// Join our lively Discord community: https://discord.gg/gCyBu8T
+	}
+}
