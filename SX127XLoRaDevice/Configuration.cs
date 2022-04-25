68413491a99856cbde3dfd059c2a8b3215fcb6bc
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2022-04-25 15:07:56 +1200

    Fixed a spelling mistake ValidHeadrerMask and removed no longer used enum value

--- SX127XLoRaDevice/Configuration.cs
+++ SX127XLoRaDevice/Configuration.cs
@@ -228,7 +228,7 @@ namespace devMobile.IoT.SX127xLoRaDevice
 			RxTimeoutMask = 0b10000000,
 			RxDoneMask = 0b01000000,
 			PayLoadCrcErrorMask = 0b00100000,
-			ValidHeadrerMask = 0b00010000,
+			ValidHeaderMask = 0b00010000,
 			TxDoneMask = 0b00001000,
 			CadDoneMask = 0b00000100,
 			FhssChangeChannelMask = 0b00000010,
@@ -238,15 +238,15 @@ namespace devMobile.IoT.SX127xLoRaDevice
 		[Flags]
 		internal enum RegIrqFlags : byte
 		{
+			ClearNone = 0b00000000,
 			RxTimeout = 0b10000000,
 			RxDone = 0b01000000,
 			PayLoadCrcError = 0b00100000,
-			ValidHeadrer = 0b00010000,
+			ValidHeader = 0b00010000,
 			TxDone = 0b00001000,
 			CadDone = 0b00000100,
 			FhssChangeChannel = 0b00000010,
 			CadDetected = 0b00000001,
-			ClearAll = 0b11111111,
 		}
 
 		[Flags]
